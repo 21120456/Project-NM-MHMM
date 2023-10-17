160947ca984102bfd14e3466626d5a35aff3c62b
HG <kthoangst1@gmail.com>
2023-10-17 22:08:24 +0700

    Add Function 		isNumber2or3 		isEvenNumber 		findSandR Signed-off-by: HG <kthoangst1@gmail.com>

--- Project_01/Project_01_01/Main.cpp
+++ Project_01/Project_01_01/Main.cpp
@@ -3,135 +3,206 @@
 #include<vector>
 using namespace std;
 
-vector<int> convertHexToBinary(string s) 
+/*
+Chuyen chuoi Hex sang chuoi Binary
+Input:	1 string ( chuoi Hex ) ( strHex ).
+Output: 1 vector ( chuoi Binary ).
+*/
+vector<int> convertHexToBinary(string strHex) 
 {
-	vector<int> b;
-	for (int i = 0; i < s.length(); i++)
+	vector<int> arrBit;
+	for (int i = 0; i < strHex.length(); i++)
 	{
-		switch (s[i])
+		switch (strHex[i])
 		{
 		case '0': {
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
 			break;
 		}
 		case '1': {
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
 			break;
 		}
 		case '2': {
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
 			break;
 		}
 		case '3': {
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
 			break;
 		}
 		case '4': {
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
 			break;
 		}
 		case '5': {
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
 			break;
 		}
 		case '6': {
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
 			break;
 		}
 		case '7': {
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
 			break;
 		}
 		case '8': {
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
 			break;
 		}
 		case '9': {
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
 			break;
 		}
 		case 'A': {
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
 			break;
 		}
 		case 'B': {
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
 			break;
 		}
 		case 'C': {
-			b.push_back(0);
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
 			break;
 		}
 		case 'D': {
-			b.push_back(1);
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
 			break;
 		}
 		case 'E': {
-			b.push_back(0);
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(1);
+			arrBit.push_back(0);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
 			break;
 		}
 		case 'F': {
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(1);
-			b.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
+			arrBit.push_back(1);
 			break;
 		}
 		}
 	}
-	return b;
+	return arrBit;
 }
 
-int main() 
+/*
+Kiem tra chuoi Hex co phai la 2 hoac la 3 khong ?
+*	Mo rong:	Kiem tra co phai la 0 hoac 1 khong ?
+Input:	1 string ( chuoi Hex ) ( strHex ).
+Output:	1 trong 3 gia tri ( 0, 1, 2 ).
+		*	1:	Chuoi Hex la 2 hoac la 3.
+		*	2:	Chuoi Hex la 0 hoac la 1.
+		*	0:	Chuoi Hex khong thuoc cac truong hop tren.
+*/
+int isNumber2or3(string strHex) 
 {
-	string s;
-	vector<int> b;
-	cin >> s;
-	b = convertHexToBinary(s);
+	if (strHex.length() != 1) return 0;
+	if (strHex == "0" || strHex == "1") return 2;
+	if (strHex == "2" || strHex == "3") return 1;
+	return 0;
+}
+
+/*
+Kiem tra chuoi Hex co phai la so chan khong ?
+Input:	1 string ( chuoi Hex ) ( strHex ).
+Output:	True hoac False.
+		*	True:	Chuoi Hex la 1 so chan. 
+		*	False:	Chuoi Hex la 1 so le. 
+*/
+bool isEvenNumber(string strHex) 
+{
+	char tempStrHex = strHex[strHex.length() - 1];
+	if (tempStrHex == '0' || tempStrHex == '2' || tempStrHex == '4' || tempStrHex == '6'
+		|| tempStrHex == '8' || tempStrHex == 'A' || tempStrHex == 'C' || tempStrHex == 'E')
+		return true;
+	return false;
+}
+
+/*
+Tim s va r trong:
+n - 1 = ( 2 ^ s ) * r. Voi r la 1 so le.
+
+Input:	1 vector ( chuoi Binary ) ( arrBit ), 1 so nguyen ( s )
+		, 1 vector ( chuoi Binary ) ( arrBitR ).
+Output:	
+*/
+void findSandR(vector<int> arrBit, int& s, vector<int>& arrBitR)
+{
+	s = 0;
+	vector<int> tempArrBit = arrBit;
+	tempArrBit[0] = 0;
+	while (tempArrBit[0] == 0) 
+	{
+		s++;
+		tempArrBit.erase(tempArrBit.begin());
+	}
+	arrBitR = tempArrBit;
+}
+
+int main()
+{
+	string strHex;
+	vector<int> arrBit;
+	int s;
+	vector<int> arrBitR;
+	cin >> strHex;
+	arrBit = convertHexToBinary(strHex);
+	if (isNumber2or3(strHex) == 1)
+	{
+		cout << 1; return 0;
+	}
+	if (isNumber2or3(strHex) == 2 || isEvenNumber(strHex))
+	{
+		cout << 0; return 0;
+	}
+	findSandR(arrBit, s, arrBitR);
+
 	return 0;
 }
